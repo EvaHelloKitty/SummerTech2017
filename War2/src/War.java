0ad615fd0c22ad11926f5fa8aa2fb389ace70941
Steven J Henderson <sjhenderson@sei.cmu.edu>
2017-07-11 00:11:30 -0400

    War2 ready

--- War2/src/War.java
+++ War2/src/War.java
@@ -2,74 +2,208 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class War {
+	
+	
+	public final static boolean INTERACTIVE = false;
+	
+	/**
+	 * The pot of cards on the table.  The winner gets these
+	 */
+	static ArrayList<Card> pot;
+	
+	static ArrayList<Card> playerAhand;
+	static ArrayList<Card> playerBhand;
+	
+	static Scanner scan;
+	
+	
+	
+	
+	private static void printPot() {
+		
+		
+		System.out.println("Current Pot:");
+		for(int i=0; i<pot.size(); i++) {
+			System.out.print(pot.get(i) + ",");
+			
+			
+		}
+		System.out.println("----------------------");
+		
+		
+	}
+	
+	
+	/**
+	 * This is the Tit for Tat state of the game.
+	 * 
+	 */
+	private static void skirmishState() {
+		
+		Card lastA = null;
+		Card lastB = null;
+		
+		System.out.println("Player A, input a random character to draw");
+		
+		String not ="";
+		
+		if (INTERACTIVE) not = scan.next();
+		
+		if(playerAhand.isEmpty()) {
+			gameOver("PlayerA");
+		} else {
+			lastA = playerAhand.remove(0);
+			pot.add(lastA);
+		}
+		
+		System.out.println("Player B, input a random character to draw");
+	
+		if (INTERACTIVE) not = scan.next();
+		
+		
+		if(playerBhand.isEmpty()) {
+			gameOver("PlayerB");
+		} else {
+			lastB = playerBhand.remove(0);
+			pot.add(lastB);
+		}
+		
+		
+		System.out.println("Player A throws down a " + lastA);
+		System.out.println("Player B throws down a " + lastB);
+		
+		
+		printPot();
+	
+		if (lastA.getNumber()>lastB.getNumber()) {
+			System.out.println("Player A won the cards!");
+			playerAhand.addAll(pot);
+			pot.clear();
+		} else if (lastB.getNumber()>lastA.getNumber()) {
+			System.out.println("Player B won the cards!");
+			playerBhand.addAll(pot);
+			pot.clear();
+		} else {
+			warState();
+		}
+		
+		
+		System.out.println("Player A has " + playerAhand.size() + " cards remaining!");
+		System.out.println("Player B has " + playerBhand.size() + " cards remaining!");
+		
+		
+		skirmishState();
+		
+		
+	}
+	
+	
+	private static void warState() {
+		System.out.println("WAR!!!!");
 
+		if(playerAhand.size() < 3) {
+			gameOver("PlayerA");
+		} else {
+			
+			Card lastA = null;
+			for(int i = 0; i < 3; i++) {
+				lastA = playerAhand.remove(0);
+				pot.add(lastA);
+				
+			}
+			System.out.print("PLayer A threw down 3 cards....");
+		}
+		if(playerBhand.size() < 3) {
+			gameOver("PlayerB");
+		} else {
+			
+			Card lastB = null;
+			for(int i = 0; i < 3; i++) {
+				lastB = playerBhand.remove(0);
+				pot.add(lastB);
+				
+			}
+			System.out.print("PLayer B threw down 3 cards....");
+		}
+		skirmishState();
+		
+	}
+
+	
+	private static void gameOver(String loser) {
+		System.out.println(loser + " is out of cards!!! GAME OVER");
+		//Hard exit
+		System.exit(0);
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Card> yourHand= new ArrayList<Card>();
+		
+		
+		int place=0;
+		System.out.println("Welcome to War!");
+		scan = new Scanner(System.in);
+		boolean quit=false;
+		
+		//Make an empty pot
+		pot = new ArrayList();
+		
+		
+		
+		//Deal the hands
+		playerAhand= new ArrayList<Card>();
+		playerBhand= new ArrayList<Card>();
+		
 		CardDeck newDeck = new CardDeck();
 		newDeck=Shuffle.shuffle(newDeck);
-		ArrayList<Card> Player1War= new ArrayList<Card>();
-		ArrayList<Card> Player2War= new ArrayList<Card>();
+		
+		
 		for (int x=0;x<26;x++) {
-			yourHand.add(newDeck.deck[x]);
+			playerAhand.add(newDeck.deck[x]);
 		}
 		ArrayList<Card> dealerHand= new ArrayList<Card>();
 		for (int y=26;y<52;y++) {
-			dealerHand.add(newDeck.deck[y]);
+			playerBhand.add(newDeck.deck[y]);
 		}
-		int place=0;
-		System.out.println("Welcome to War!");
-		Scanner scan = new Scanner(System.in);
-		boolean quit=false;
-		while (!quit) {
-			System.out.println("Player 1, input a random character to draw");
-			String not = scan.next();
-			System.out.println("Player1 drew a "+yourHand.get(place));
-			System.out.println(place);
-			System.out.println("Player 2, input a random character to draw");
-			String not2 = scan.next();
-			System.out.println("Player 2 drew a "+dealerHand.get(place));
-			System.out.println(place);
-			if (yourHand.get(place).getNumber()>dealerHand.get(place).getNumber()) {
-				for (int x=0;x<=place;x++) {
-					yourHand.add(dealerHand.get(0));
-					dealerHand.remove(0);
-					yourHand.add(yourHand.get(0));
-					yourHand.remove(0);
-				}
-				place=0;
-				System.out.println("Player 1 won the cards!");
-			}
-			else if (yourHand.get(place).getNumber()<dealerHand.get(place).getNumber()) {
-				Player1War.add(yourHand.get(place));
-				
-				/*for (int x=0;x<=Player2War.size();x++) {
-					yourHand.add(Player2War.get(x));
-					dealerHand.remove(place);
-					yourHand.add(yourHand.get(place));
-					yourHand.remove(place);
-					place=0;
-				}
-				*/
-				System.out.println("Player 2 won the cards!");
-			}
-			else if (yourHand.get(place).getNumber()==yourHand.get(place).getNumber()) {
-				System.out.println("War!");
-				Player1War.add(yourHand.get(place));
-				Player1War.add(yourHand.get(place+1));
-				Player1War.add(yourHand.get(place+2));
-				Player1War.add(yourHand.get(place+3));
-				Player1War.add(yourHand.get(place+4));
-				//System.out.println("Player 1 drew a "+yourHand.get(place+4));
-				Player2War.add(dealerHand.get(place));
-				Player2War.add(dealerHand.get(place+1));
-				Player2War.add(dealerHand.get(place+2));
-				Player2War.add(dealerHand.get(place+3));
-				Player2War.add(dealerHand.get(place+4));
-				//System.out.println("Player 2 drew a "+dealerHand.get(place+4));
-				place+=4;
-			}
-		}
+		
+		//Launch the game in skirmish mode
+		skirmishState();
+		
+		//STATE0;   Deck divided
+		
+		
+		//STATE1:  card for card
+		
+		//11:PlayerA adds next card in the the Pot
+		
+		//12:PlayerB adds next card in the the Pot
+		
+		//13: Cards are compared
+		
+		//14: Clear cut winner -->  winner gets Pot;  Pot emptied; goto 11
+		
+		//15:  Else Goto Melee STATE
+		
+		
+		//STATE 2:  War round
+		
+		//21:  Player A add three to  Pot (down)
+		//22:  Player B adds three to Pot (down)
+		//24:  Player A adds one more to pot (up)
+		//25:  PLayer B adds one more to pot (up)
+		
+		//26:  Last two cards compared.  Winner gets pot.  Goto 11
+		//27:  If tie; goto 21
+		
+		
+		//Corner cases?  What if can't support all 4 cards in Melee?
+		
+		//STATE 4:  Player exhausts all cards.  Game over.
+		
+		
+		
+		
 	}
 
 }
